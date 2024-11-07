cc6b73dfdfa10248f784e50c714dac47dc943a2c
redssy <redlemagic@hotmail.com>
2024-11-06 21:33:26 -0300

    subindo projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="container h1">Ola</h1>
+    </>
+  )
+}
+
+export default App
