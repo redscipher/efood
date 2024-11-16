3a1c57198a108b8f95bcd7d8848162ac9c66098f
redssy <redlemagic@hotmail.com>
2024-11-15 22:53:03 -0300

    ajustes realizados

--- src/componentes/sessoesCompras/paginasSessoes/finalizacao/finalizacao.tsx
+++ src/componentes/sessoesCompras/paginasSessoes/finalizacao/finalizacao.tsx
@@ -1,18 +1,15 @@
 // importacoes
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Botao, Descritivo, Titulo } from '../../../../globais'
 import * as E from './estilos'
 import { abrirFechar, esvaziar } from '../../../../armazem/redutores/carrinho'
-import { RootReducer } from '../../../../armazem'
 import { confirmarPedido } from '../../../../armazem/redutores/pedidos'
 
 const Finalizacao = () => {
   // despacho de acoes p/ armazem
   const despacho = useDispatch()
-  // busca ultimo id
-  const { itens } = useSelector((estado: RootReducer) => estado.pedidos)
   // variaveis
-  const ID: string = (itens.length + 1) as unknown as string
+  const ID: string = ''
 
   // funcoes
   const fecharCarrinho = () => {
@@ -29,22 +26,26 @@ const Finalizacao = () => {
   // def retorno
   return (
     <E.default>
-      <Titulo>Pedido realizado - {ID.toString().padStart(7, '0')}</Titulo>
+      <Titulo>
+        Pedido {ID !== '' ? 'realizado' : ''} - {ID.toString().padStart(7, '0')}
+      </Titulo>
       <div>
-        <Descritivo tamanho={14}>
-          Estamos felizes em informar que seu pedido já está em processo de
-          preparação e, em breve, será entregue no endereço fornecido. <br />
-          <br />
-          Gostaríamos de ressaltar que nossos entregadores não estão autorizados
-          a realizar cobranças extras. <br />
-          <br />
-          Lembre-se da importância de higienizar as mãos após o recebimento do
-          pedido, garantindo assim sua segurança e bem-estar durante a refeição.{' '}
-          <br />
-          <br />
-          Esperamos que desfrute de uma deliciosa e agradável experiência
-          gastronômica. Bom apetite!
-        </Descritivo>
+        {ID !== '' && (
+          <Descritivo tamanho={14}>
+            Estamos felizes em informar que seu pedido já está em processo de
+            preparação e, em breve, será entregue no endereço fornecido. <br />
+            <br />
+            Gostaríamos de ressaltar que nossos entregadores não estão
+            autorizados a realizar cobranças extras. <br />
+            <br />
+            Lembre-se da importância de higienizar as mãos após o recebimento do
+            pedido, garantindo assim sua segurança e bem-estar durante a
+            refeição. <br />
+            <br />
+            Esperamos que desfrute de uma deliciosa e agradável experiência
+            gastronômica. Bom apetite!
+          </Descritivo>
+        )}
         <Botao type="button" onClick={finalizarPedido}>
           Concluir
         </Botao>
